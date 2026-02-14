dd96dbcf0d262697332a24af3b84c7c0bec34beb
nudemeth <7686682+nudemeth@users.noreply.github.com>
2026-02-14 16:48:47 +0700

    Rollback to non-persistence due to localstack community edition limitation.

--- ci/terraform/account-event-listener-lambda.tf
+++ ci/terraform/account-event-listener-lambda.tf
@@ -1,10 +1,11 @@
 resource "aws_lambda_function" "account_event_listener" {
-  function_name = "account-event-listener"
-  role          = "arn:aws:iam::000000000000:role/account-event-listener-role"
-  handler       = "AccountEventListener"
-  runtime       = "dotnet8"
-  filename      = "./AccountEventListener.zip"
-  publish       = true
+  function_name    = "account-event-listener"
+  role             = "arn:aws:iam::000000000000:role/account-event-listener-role"
+  handler          = "AccountEventListener"
+  runtime          = "dotnet8"
+  filename         = "./AccountEventListener.zip"
+  source_code_hash = filebase64sha256("./AccountEventListener.zip")
+  publish          = true
 }
 
 resource "aws_lambda_event_source_mapping" "dynamodb_to_lambda" {
